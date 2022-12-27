c542357da9fbc2cf6bce044af2700e5f9b4ac077
Roberto Bertolini <robertoalessandro.bertolini@mail.polimi.it>
2022-12-27 21:21:24 +0100

    Cleanup repo
    Not really clean

--- pynqrypt-vitis-hls/pynqrypt/pynqrypt/.autopilot/db/coregen/control.slave.tcl
+++
@@ -1,4 +0,0 @@
-dict set slaves control {ports {key {type i_ap_none width 128} nonce {type i_ap_none width 96} plaintext_length {type i_ap_none width 64} plaintext {type i_ap_none width 64} ciphertext {type i_ap_none width 64} ap_start {type ap_ctrl width 1} ap_done {type ap_ctrl width 1} ap_ready {type ap_ctrl width 1} ap_idle {type ap_ctrl width 1}} mems {} has_ctrl 1}
-set datawidth 32
-set addrwidth 64
-set intr_clr_mode TOW
--- pynqrypt-vitis-hls/pynqrypt/pynqrypt/.autopilot/db/hls_design_meta.tcl
+++
@@ -1,2 +0,0 @@
-set design_latency 27751
-set design_II 27751
